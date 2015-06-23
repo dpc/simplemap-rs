bef6bd02a4ae08d1b1810b3cb3ec2fafb2af0ce2
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-06-22 23:19:45 -0800

    Update documentation

---
+++ implementors/core/ops/trait.IndexMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['simplemap'] = ["impl&lt;Idx, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;Idx&gt; for <a class='struct' href='simplemap/struct.SimpleMap.html' title='simplemap::SimpleMap'>SimpleMap</a>&lt;Idx, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
