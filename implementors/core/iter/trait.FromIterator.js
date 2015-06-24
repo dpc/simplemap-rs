10c46db44cbfd4aaf0ef38dd5d29033298395843
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-06-23 21:28:51 -0800

    Update documentation

---
+++ implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['simplemap'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(K, V)</a>&gt; for <a class='struct' href='simplemap/struct.SimpleMap.html' title='simplemap::SimpleMap'>SimpleMap</a>&lt;K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/core/ops/trait.Index.js
+++ implementors/core/ops/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['simplemap'] = ["impl&lt;Idx, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;Idx&gt; for <a class='struct' href='simplemap/struct.SimpleMap.html' title='simplemap::SimpleMap'>SimpleMap</a>&lt;Idx, T&gt;",];
+implementors['simplemap'] = ["impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;K&gt; for <a class='struct' href='simplemap/struct.SimpleMap.html' title='simplemap::SimpleMap'>SimpleMap</a>&lt;K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
